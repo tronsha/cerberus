cc6ed24fbf1dbc40245b25aa00adb5824fef11ff
Stefan Hüsges <tronsha@gmail.com>
2013-03-31 20:11:28 +0200

    Reformat Code

--- bot.php
+++ bot.php
@@ -3,8 +3,9 @@
 
 set_time_limit(0);
 
-if(version_compare(phpversion(), '5.1', '>=') === true)
+if (version_compare(phpversion(), '5.1', '>=') === true) {
     date_default_timezone_set('Europe/Berlin');
+}
 
 require_once('./library/function.php');
 require_once('./library/db.class.php');
