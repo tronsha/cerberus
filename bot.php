c287f6254792eb08a02528582b4bb522afe20155
Stefan Hüsges <tronsha@gmail.com>
2014-06-03 18:33:49 +0200

    PHP 5.3.2 or higher is required

--- bot.php
+++ bot.php
@@ -1,9 +1,14 @@
 #! /usr/bin/php5
 <?php
 
-if (version_compare(phpversion(), '5.1', '>=') === true) {
-    date_default_timezone_set('Europe/Berlin');
+if (version_compare(phpversion(), '5.3.2', '<') === true) {
+    echo 'Your version of PHP is ' . phpversion() . PHP_EOL;
+    echo 'PHP 5.3.2 or higher is required' . PHP_EOL;
+    exit;
 }
+
+error_reporting(-1);
+date_default_timezone_set('Europe/Berlin');
 
 require_once("vendor/autoload.php");
 
