4629f9194c5eb3c9042b8353510840f1cfcdddd4
Stefan Hüsges <tronsha@gmx.de>
2015-02-20 12:47:45 +0100

    added formatter tests

--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -41,20 +41,211 @@ class FormatterTest extends \PHPUnit_Framework_TestCase
 
     public function testConsoleBold()
     {
-        $this->assertEquals("\033[1mfoo\033[22m", $this->consoleFormatter->bold("\x02foo\x02"));
-        $this->assertEquals("\033[1mfoo\033[22m", $this->consoleFormatter->bold("\x02foo"));
+        $this->assertEquals(
+            "\033[1mfoo\033[22m",
+            $this->consoleFormatter->bold("\x02foo\x02")
+        );
+        $this->assertEquals(
+            "\033[1mfoo\033[22m",
+            $this->consoleFormatter->bold("\x02foo")
+        );
     }
 
     public function testConsoleUnderline()
     {
-        $this->assertEquals("\033[4mfoo\033[24m", $this->consoleFormatter->underline("\x1Ffoo\x1F"));
-        $this->assertEquals("\033[4mfoo\033[24m", $this->consoleFormatter->underline("\x1Ffoo"));
+        $this->assertEquals(
+            "\033[4mfoo\033[24m",
+            $this->consoleFormatter->underline("\x1Ffoo\x1F")
+        );
+        $this->assertEquals(
+            "\033[4mfoo\033[24m",
+            $this->consoleFormatter->underline("\x1Ffoo")
+        );
     }
 
     public function testConsoleColor()
     {
-        $this->assertEquals("\033[38;5;0;48;5;11mfoobar\033[39;49m", $this->consoleFormatter->color("\x03" . '1,8foobar' . "\x03"));
-        $this->assertEquals("\033[38;5;0;48;5;11mfoobar\033[39;49m", $this->consoleFormatter->color("\x03" . '1,8foobar'));
-        $this->assertEquals("\033[38;5;0;48;5;11mfoo\033[39;49mbar", $this->consoleFormatter->color("\x03" . '1,8foo' . "\x03" . 'bar'));
+        $this->assertEquals(
+            "\033[38;5;15mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '0foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;0mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '1foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;4mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '2foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;2mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '3foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;9mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '4foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;1mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '5foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;5mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '6foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;3mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '7foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;11mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '8foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;10mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '9foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;6mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '10foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;14mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '11foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;12mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '12foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;13mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '13foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;8mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '14foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;7mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '15foo' . "\x03")
+        );
+
+        $this->assertEquals(
+            "\033[38;5;15;48;5;0mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '0,1foo' . "\x03")
+        );
+        $this->assertEquals(
+            "\033[38;5;15;48;5;0mfoo\033[39;49m",
+            $this->consoleFormatter->color("\x03" . '0,1foo')
+        );
+        $this->assertEquals(
+            "\033[38;5;15;48;5;0mfoo\033[39;49mbar",
+            $this->consoleFormatter->color("\x03" . '0,1foo' . "\x03" . 'bar')
+        );
+    }
+
+    public function testHtmlBold()
+    {
+        $this->assertEquals(
+            '<span style="font-weight: bold;">foo</span>',
+            $this->htmlFormatter->bold("\x02foo\x02")
+        );
+        $this->assertEquals(
+            '<span style="font-weight: bold;">foo</span>',
+            $this->htmlFormatter->bold("\x02foo")
+        );
+    }
+
+    public function testHtmlUnderline()
+    {
+        $this->assertEquals(
+            '<span style="text-decoration: underline;">foo</span>',
+            $this->htmlFormatter->underline("\x1Ffoo\x1F")
+        );
+        $this->assertEquals(
+            '<span style="text-decoration: underline;">foo</span>',
+            $this->htmlFormatter->underline("\x1Ffoo")
+        );
+    }
+
+    public function testHtmlColor()
+    {
+        $this->assertEquals(
+            '<span style="color: #FFFFFF;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '0foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #000000;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '1foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #00007F;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '2foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #009300;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '3foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #FF0000;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '4foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #7F0000;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '5foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #9C009C;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '6foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #FC7F00;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '7foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #FFFF00;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '8foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #00FC00;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '9foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #009393;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '10foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #00FFFF;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '11foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #0000FC;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '12foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #FF00FF;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '13foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #7F7F7F;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '14foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #D2D2D2;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '15foo' . "\x03")
+        );
+
+        $this->assertEquals(
+            '<span style="color: #FFFFFF; background-color: #000000;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '0,1foo' . "\x03")
+        );
+        $this->assertEquals(
+            '<span style="color: #FFFFFF; background-color: #000000;">foo</span>',
+            $this->htmlFormatter->color("\x03" . '0,1foo')
+        );
+        $this->assertEquals(
+            '<span style="color: #FFFFFF; background-color: #000000;">foo</span>bar',
+            $this->htmlFormatter->color("\x03" . '0,1foo' . "\x03" . 'bar')
+        );
     }
 }
