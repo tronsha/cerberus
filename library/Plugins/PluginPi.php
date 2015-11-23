6ac725d1948a74c12c9fc699ec2d75702904b84a
Stefan Hüsges <Tronsha@gmx.de>
2015-11-23 07:57:28 +0100

    Update PluginPi.php

--- library/Plugins/PluginPi.php
+++ library/Plugins/PluginPi.php
@@ -32,6 +32,7 @@ use Cerberus\Plugin;
  * @link http://www.raspberrypi.org/ Raspberry Pi
  * @link http://wiringpi.com/ WiringPi
  * @link https://github.com/tronsha/wiringPi WiringPi GIT Fork
+ * @link https://github.com/technion/lol_dht22 DHT 22
  * @license http://www.gnu.org/licenses/gpl-3.0 GNU General Public License
  */
 class PluginPi extends Plugin
