51e06190206e5512a8599d5cf251f9954a77a390
Stefan Hüsges <tronsha@gmx.de>
2015-12-15 10:16:16 +0100

    PluginControl

---
+++ library/Plugins/PluginControl.php
@@ -0,0 +1,49 @@
+<?php
+
+/*   Cerberus IRCBot
+ *   Copyright (C) 2008 - 2015 Stefan Hüsges
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Cerberus\Plugins;
+
+use Cerberus\Cerberus;
+use Cerberus\Plugin;
+
+/**
+ * Class PluginControl
+ * @package Cerberus\Plugins
+ * @author Stefan Hüsges
+ */
+class PluginControl extends Plugin
+{
+    /**
+     *
+     */
+    protected function init()
+    {
+        $this->irc->addEvent('onTick', $this);
+    }
+
+    /**
+     * @return bool
+     */
+    public function onTick()
+    {
+        $returnValue = parent::onLoad();
+
+        return $returnValue;
+    }
+}
