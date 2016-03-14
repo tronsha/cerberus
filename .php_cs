06cc9aa3bb3c0e4f8e2fa7dde24977b53a232b06
Stefan Hüsges <tronsha@gmx.de>
2016-03-14 16:05:58 +0100

    php-cs-fixer

--- .php_cs
+++ .php_cs
@@ -1,10 +1,29 @@
 <?php
 
+$header = <<<'EOF'
+Cerberus IRCBot
+Copyright (C) 2008 - 2016 Stefan Hüsges
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation; either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, see <http://www.gnu.org/licenses/>.
+EOF;
+
 return PhpCsFixer\Config::create()
     ->setRiskyAllowed(true)
     ->setRules(array(
         '@PSR2' => true,
         'combine_consecutive_unsets' => true,
+        'header_comment' => array('header' => $header),
         'long_array_syntax' => false,
         'no_useless_return' => true,
         'ordered_imports' => true,
--- bin/bot.php
+++ bin/bot.php
@@ -1,21 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-/*   Cerberus IRCBot
- *   Copyright (C) 2008 - 2016 Stefan Hüsges
+/*
+ * Cerberus IRCBot
+ * Copyright (C) 2008 - 2016 Stefan Hüsges
  *
- *   This program is free software; you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by the Free
- *   Software Foundation; either version 3 of the License, or (at your option)
- *   any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.
  *
- *   This program is distributed in the hope that it will be useful, but
- *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
  *
- *   You should have received a copy of the GNU General Public License along
- *   with this program; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 if (version_compare(phpversion(), '5.4.0', '<') === true) {
