c973fbd1c82585a9a59e179469aa0565a739f5b7
Stefan Hüsges <tronsha@gmx.de>
2016-05-11 08:58:16 +0200

    update DbTest

--- tests/DbTest.php
+++ tests/DbTest.php
@@ -27,6 +27,7 @@ class DbTest extends \PHPUnit_Framework_TestCase
     protected static $config;
     protected static $database;
     protected $db;
+    protected $irc;
 
     public static function setUpBeforeClass()
     {
@@ -46,10 +47,15 @@ class DbTest extends \PHPUnit_Framework_TestCase
         $this->db = DriverManager::getConnection(self::$config['testdb']);
         $this->db->query(file_get_contents(Cerberus::getPath() . '/cerberus.mysql.sql'));
         self::$config['db'] = self::$config['testdb'];
+        $this->irc = new Irc(self::$config);
+        $this->irc->getConsole()->output(false);
+        $this->irc->init();
+        $this->invokeMethod($this->irc, 'loadPlugin', 'test');
     }
 
     protected function tearDown()
     {
+        unset($this->irc);
         if ($this->db === null) {
             $this->fail('No connection to database...');
         }
@@ -58,8 +64,32 @@ class DbTest extends \PHPUnit_Framework_TestCase
         $this->db->close();
     }
 
-    public function testDb()
+    public function invokeMethod(&$object, $methodName)
     {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        $parameters = array_slice(func_get_args(), 2);
 
+        return $method->invokeArgs($object, $parameters);
+    }
+
+    public function testUserInChannel()
+    {
+        $db = $this->irc->getDb();
+        $db->addUserToChannel('#test', 'test');
+        $user = $db->getUserInChannel('#test', 'test');
+        $this->assertSame('test', $user[0]['username']);
+    }
+
+    public function testUserInChannelBackslash()
+    {
+        $db = $this->irc->getDb();
+        $db->addUserToChannel('#test', 'foo\\\\bar');
+        $user = $db->getUserInChannel('#test', 'foo\\\\bar');
+        $this->assertSame('foo\\\\bar', $user[0]['username']);
+        $db->removeUser('foo\\\\bar');
+        $user = $db->getUserInChannel('#test', 'foo\\\\bar');
+        $this->assertEmpty($user);
     }
 }
