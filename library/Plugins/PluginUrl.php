61c22699728231d78154dba84f6f04c7a6e0d7ac
Stefan Hüsges <tronsha@gmx.de>
2017-09-12 12:20:51 +0200

    update

--- library/Plugins/PluginUrl.php
+++ library/Plugins/PluginUrl.php
@@ -37,6 +37,7 @@ class PluginUrl extends Plugin
     protected function init()
     {
         $this->addEvent('onPrivmsg');
+        $this->addEvent('on322');
         $this->file = realpath($this->getConfig()->getLogfiledirectory()) . '/url.txt';
     }
 
@@ -63,6 +64,16 @@ class PluginUrl extends Plugin
     }
 
     /**
+     * @param array $data
+     * @return bool
+     */
+    public function on322($data)
+    {
+        $urls = $this->parseUrls($data['topic']);
+        $this->writeToFile($urls);
+    }
+
+    /**
      * @param string $text
      * @return array
      */
@@ -70,7 +81,7 @@ class PluginUrl extends Plugin
     {
         $urls = [];
         $pairs = ['(' => ')', '<' => '>', '[' => ']', '{' => '}'];
-        preg_match_all('/(?:^|\s)([\"\'\(\<\[\{])?\s*([a-zA-Z]+:\/\/\S+)(?:\s|$)/si', $text, $matches, PREG_SET_ORDER);
+        preg_match_all('/(?:^|\s)([\"\'\(\<\[\{])?\s*([a-zA-Z]+:\/\/\S+)(?:\s|\xC2\xA0|$)/si', $text, $matches, PREG_SET_ORDER);
         foreach ($matches as $match) {
             $charlist = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x20\x2C\x2E\x7F";
             $charlist .= '“”';
