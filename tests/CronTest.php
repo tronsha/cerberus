e93a535e80885775ad7122e92d1ecaa782655f3a
Stefan Hüsges <tronsha@gmx.de>
2017-10-21 12:35:35 +0200

    update cron testCompare

--- tests/CronTest.php
+++ tests/CronTest.php
@@ -34,66 +34,70 @@ class CronTest extends \PHPUnit_Framework_TestCase
         unset($this->cron);
     }
 
-    public function testCompare()
+    /**
+     * @param boolean $expected
+     * @param string $cronString
+     * @param integer $minute
+     * @param integer $hour
+     * @param integer $day_of_month
+     * @param integer $month
+     * @param integer $day_of_week
+     *
+     * @dataProvider compareProvider
+     */
+    public function testCompare($expected, $cronString, $minute, $hour, $day_of_month, $month, $day_of_week)
     {
-        $this->assertTrue($this->cron->compare('* * * * *', 15, 12, 1, 1, 0));
+        $this->assertSame($expected, $this->cron->compare($cronString, $minute, $hour, $day_of_month, $month, $day_of_week));
+    }
 
-        $this->assertTrue($this->cron->compare('15 * * * *', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* 12 * * *', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* * 1 * *', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* * * 1 *', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* * * * 0', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* * * * 7', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('15 12 * * *', 15, 12, 1, 1, 0));
-
-        $this->assertFalse($this->cron->compare('30 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* 13 * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* * 2 * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* * * 2 *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* * * * 1', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('15 13 * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('30 12 * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('* * 1 * 1', 15, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('* * 2 * 0', 15, 12, 1, 1, 0));
-
-        $this->assertFalse($this->cron->compare('* * 2 * 1', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* * 1 2 1', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('* * 2 2 0', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0-29 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('30-59 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0,15,30,45 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('0,20,40 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('10-20,30-40 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('20-30,40-50 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('*/5 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('*/10 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0-30/5 * * * *', 15, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('30-55/5 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('2-32/5 * * * *', 17, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('2-32/5 * * * *', 15, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('1-20/3 * * * *', 1, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('1-20/3 * * * *', 4, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('1-20/3 * * * *', 3, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0 */6 * * *', 0, 0, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('0 */6 * * *', 0, 6, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('0 */6 * * *', 0, 12, 1, 1, 0));
-        $this->assertTrue($this->cron->compare('0 */6 * * *', 0, 18, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('0 */6 * * *', 0, 14, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0 12 * * SUN', 0, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('0 12 * * SAT', 0, 12, 1, 1, 0));
-
-        $this->assertTrue($this->cron->compare('0 12 * JAN-JUN *', 0, 12, 1, 1, 0));
-        $this->assertFalse($this->cron->compare('0 12 * JUL-DEC *', 0, 12, 1, 1, 0));
+    public function compareProvider()
+    {
+        return [
+            [true, '* * * * *', 15, 12, 1, 1, 0],
+            [true, '15 * * * *', 15, 12, 1, 1, 0],
+            [true, '* 12 * * *', 15, 12, 1, 1, 0],
+            [true, '* * 1 * *', 15, 12, 1, 1, 0],
+            [true, '* * * 1 *', 15, 12, 1, 1, 0],
+            [true, '* * * * 0', 15, 12, 1, 1, 0],
+            [true, '* * * * 7', 15, 12, 1, 1, 0],
+            [true, '15 12 * * *', 15, 12, 1, 1, 0],
+            [false, '30 * * * *', 15, 12, 1, 1, 0],
+            [false, '* 13 * * *', 15, 12, 1, 1, 0],
+            [false, '* * 2 * *', 15, 12, 1, 1, 0],
+            [false, '* * * 2 *', 15, 12, 1, 1, 0],
+            [false, '* * * * 1', 15, 12, 1, 1, 0],
+            [false, '15 13 * * *', 15, 12, 1, 1, 0],
+            [false, '30 12 * * *', 15, 12, 1, 1, 0],
+            [true, '* * 1 * 1', 15, 12, 1, 1, 0],
+            [true, '* * 2 * 0', 15, 12, 1, 1, 0],
+            [false, '* * 2 * 1', 15, 12, 1, 1, 0],
+            [false, '* * 1 2 1', 15, 12, 1, 1, 0],
+            [false, '* * 2 2 0', 15, 12, 1, 1, 0],
+            [true, '0-29 * * * *', 15, 12, 1, 1, 0],
+            [false, '30-59 * * * *', 15, 12, 1, 1, 0],
+            [true, '0,15,30,45 * * * *', 15, 12, 1, 1, 0],
+            [false, '0,20,40 * * * *', 15, 12, 1, 1, 0],
+            [true, '10-20,30-40 * * * *', 15, 12, 1, 1, 0],
+            [false, '20-30,40-50 * * * *', 15, 12, 1, 1, 0],
+            [true, '*/5 * * * *', 15, 12, 1, 1, 0],
+            [false, '*/10 * * * *', 15, 12, 1, 1, 0],
+            [true, '0-30/5 * * * *', 15, 12, 1, 1, 0],
+            [false, '30-55/5 * * * *', 15, 12, 1, 1, 0],
+            [true, '2-32/5 * * * *', 17, 12, 1, 1, 0],
+            [false, '2-32/5 * * * *', 15, 12, 1, 1, 0],
+            [true, '1-20/3 * * * *', 1, 12, 1, 1, 0],
+            [true, '1-20/3 * * * *', 4, 12, 1, 1, 0],
+            [false, '1-20/3 * * * *', 3, 12, 1, 1, 0],
+            [true, '0 */6 * * *', 0, 0, 1, 1, 0],
+            [true, '0 */6 * * *', 0, 6, 1, 1, 0],
+            [true, '0 */6 * * *', 0, 12, 1, 1, 0],
+            [true, '0 */6 * * *', 0, 18, 1, 1, 0],
+            [false, '0 */6 * * *', 0, 14, 1, 1, 0],
+            [true, '0 12 * * SUN', 0, 12, 1, 1, 0],
+            [false, '0 12 * * SAT', 0, 12, 1, 1, 0],
+            [true, '0 12 * JAN-JUN *', 0, 12, 1, 1, 0],
+            [false, '0 12 * JUL-DEC *', 0, 12, 1, 1, 0]
+        ];
     }
 
     public function testAdd()
