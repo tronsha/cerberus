d537565faff4c4d102878b300326382e964196a9
Stefan Hüsges <tronsha@gmx.de>
2017-11-05 15:18:23 +0100

    update phpunit

---
+++ phpunit.bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase'))
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+
+require_once 'vendor/autoload.php';
