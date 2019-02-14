03371c37e9cc7a8b34cebedb8033d5a81094808f
Stefan Hüsges <tronsha@gmx.de>
2019-02-14 15:15:07 +0100

    Update Copyright

--- library/Events/EventOn403.php
+++ library/Events/EventOn403.php
@@ -2,7 +2,7 @@
 
 /*
  * Cerberus IRCBot
- * Copyright (C) 2008 - 2018 Stefan Hüsges
+ * Copyright (C) 2008 - 2019 Stefan Hüsges
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
